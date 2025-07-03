60d4691437f6ed050902013c050870d181f8fb9a
João Gabriel Odilon da Silva <joao.silva41@etudante.ifms.edu.br>
2025-07-02 21:31:34 -0300

    Aula: 01/07/2025

--- actions/marca-actions.ts
+++ actions/marca-actions.ts
@@ -6,15 +6,18 @@ const API_URL = process.env.API_URL
 const headers = {
     'Content-Type': 'application/json'
 }
+ export type MaraFormstate={
+    nome:String
+}
 
-export async function criarMarca(formData:FormData){
+export async function criarMarca(prevState:MaraFormstate,formData:FormData){
         let response = await fetch(`${API_URL}/marca`,{
         headers,
         method:'POST',
         body:stringifyFormData(formData)
     })
      //console.log(await response.json())
-   
+   return prevState
     redirect('/cadastro/marcas/')
 }
 export async function deletarMarca(id:number){
@@ -23,4 +26,3 @@ export async function deletarMarca(id:number){
     })
     redirect('/cadastro/marcas/')
 }
-
--- app/cadastro/marcas/marcas-form.tsx
+++ app/cadastro/marcas/marcas-form.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { criarMarca } from "@/actions/marca-actions";
+import { criarMarca, MaraFormstate } from "@/actions/marca-actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { SaveIcon } from "lucide-react";
+import { Loader2Icon, SaveIcon } from "lucide-react";
 import Link from "next/link";
+import { useActionState } from "react";
 
 export function MarcasForm() {
+  const initialState: MaraFormstate = {
+    nome:'',
+  }
+  
+  const [state,formAction,isPending] = useActionState(criarMarca,initialState)
   return (
     <section className="mt-8">
-      <form action={criarMarca} className="max-w-2xl">
+      <form action={formAction} className="max-w-2xl">
         <div className="space-y-4">
           <Label htmlFor="nome">Nome</Label>
           <Input name="nome" />
@@ -22,7 +28,7 @@ export function MarcasForm() {
             </Button>
           </Link>
           <Button type="submit">
-            <SaveIcon />
+            {isPending ? <Loader2Icon/> : <SaveIcon />}
             Salvar
           </Button>
         </div>
